697b9793a1b55458e4debd8a8124a2a260dd521a
Дмитрий Караваев <karavaevdmit@gmail.com>
2026-05-13 23:16:11 +0800

    доделал все Задания на работу с кодом

--- Script.js
+++ Script.js
@@ -1,4 +1,54 @@
+
 let a = 10;
 alert(a);
 a = 20;
 alert(a);
+
+const date = "9 января 2007";
+alert(date);
+
+const personality = "Brendan Eich";
+alert(personality);
+
+let c =10;
+let b = 2;
+
+let sum = c + b;
+alert(sum);
+
+let difference = c - b;
+alert(difference);
+
+let product = c * b;
+alert(product);
+
+let quotient = c / b;
+alert(quotient);
+
+let power = 2 ** 5;
+alert(power);
+
+let d = 9;
+let remainder = d % b;
+alert(remainder);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+const user = {
+   name: 'Дмитрий',
+   age: '40',
+   isAdmin: true
+}
+
+let yourName = prompt('Введите ваше имя:');
+alert('Привет, ' + yourName + '!');
