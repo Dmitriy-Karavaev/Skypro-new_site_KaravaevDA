a6618c4001a98a809099e642a0900f5cdb3e9981
Дмитрий Караваев <karavaevdmit@gmail.com>
2026-08-18 20:24:46 +0800

    Добавил недостающие задачи 4, 5. Убрал паддинги у кнопки в Head

--- Script.js
+++ Script.js
@@ -92,3 +92,29 @@ setTimeout(() => {
   const elapsed = Math.floor((endTime - startTime) / 1000);
   console.log(`30 секунд прошло (прошло примерно ${elapsed} сек)`);
 }, durationMs);
+
+//Задание 4
+function delayForSecond(callback) {
+    setTimeout(callback, 1000);
+}
+
+delayForSecond(function () {
+   console.log('Привет, Глеб!');
+});
+
+//Задание 5
+// Функция delayForSecond через 1 секунду пишет в консоль 
+// «Прошла одна секунда», а затем вызывает переданный колбэк
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) {  cb(); }
+    }, 1000)
+}
+
+// Функция sayHi выводит в консоль приветствие для указанного имени
+function sayHi (name) {
+    console.log(`Привет, ${name}!`);
+}
+
+delayForSecond(() => sayHi('Глеб'));
